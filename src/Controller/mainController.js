546c75f7056a458c4a20ea55566ddf3ab6baf31c
Jinny <ggab.challenge@gmail.com>
2024-11-23 22:46:14 +0900

    feat: 주어진 횟수 동안 자동차 전진 또는 멈춤 기능 구현

--- src/Controller/mainController.js
+++ src/Controller/mainController.js
@@ -1,4 +1,5 @@
 import { parseCarNames } from '../Model/carNamesParser.js';
+import { RaceMovementHandler } from '../Model/raceMovement.js';
 import { CarNamesExceptionalHandler } from '../Validation/carNamesValidator.js';
 import { InputHandler } from '../View/inputView.js';
 
@@ -7,11 +8,16 @@ export class MainController {
     this.input = new InputHandler();
   }
 
+  // eslint-disable-next-line max-lines-per-function
   async initializeProgram() {
     const carNamesInput = await this.input.getCarNamesInput();
     const parsedCarNames = parseCarNames(carNamesInput);
     new CarNamesExceptionalHandler().validateCarNames(parsedCarNames);
 
     const racingCountInput = await this.input.getRacingCountInput();
+
+    new RaceMovementHandler().getRaceMovement(parsedCarNames, racingCountInput);
+
+    // 각 자동차가 횟수 동안 전진
   }
 }
---
+++ src/Model/raceMovement.js
@@ -0,0 +1,34 @@
+import { Random } from '@woowacourse/mission-utils';
+
+export class RaceMovementHandler {
+  // eslint-disable-next-line max-lines-per-function
+  getRaceMovement(parsedCarNames, racingCountInput) {
+    this.parsedCarNames = parsedCarNames;
+    this.racingCount = racingCountInput;
+    this.racingMovement = [];
+
+    this.InitializeArray();
+
+    for (let i = 0; i < this.racingCount; i += 1) {
+      this.MakeRandomNumberAndMovementForOnce();
+      console.log(this.racingMovement);
+    }
+  }
+
+  // TODO: 리펙토링
+  InitializeArray() {
+    for (let i = 0; i < this.parsedCarNames.length; i += 1) {
+      this.racingMovement.push(0);
+    }
+  }
+
+  // TODO: 리펙토링
+  MakeRandomNumberAndMovementForOnce() {
+    for (let j = 0; j < this.parsedCarNames.length; j += 1) {
+      const randomNumber = Random.pickNumberInRange(0, 9);
+      if (randomNumber >= 4) {
+        this.racingMovement[j] += 1;
+      }
+    }
+  }
+}
